d759c0042f0cfff565bafe2976f062b6d522e297
SHINSUNGJIN <pxp13716@gmail.com>
2022-09-26 10:51:48 +0900

    .

--- Chap01_JavaScript/src/A02_FunLiteral.js
+++ Chap01_JavaScript/src/A02_FunLiteral.js
@@ -1,26 +1,41 @@
 
-(function(){
+(function () {
 
     // ES5. 함수 리터럴
-    var sayHello = function() { 
+    var sayHello = function () {
         return 1;
     };
 
-    console.log( '1 => ' + sayHello() );
+    console.log('1 => ' + sayHello());
 
     {
-        var sayHello = function() {
+        var sayHello = function () {
             return 2;
         };
-        
-        console.log( '2 => ' + sayHello() );
+
+        console.log('2 => ' + sayHello());
     }
 
-    console.log( '3 => ' + sayHello() );
+    console.log('3 => ' + sayHello());
     console.log("");
 
 
     // ES6. Function
-    
 
+    let yourHello = function () {
+        return 1;
+    };
+
+    console.log('1 => ' + yourHello());
+
+    {
+        const yourHello = function () {
+            return 2;
+        };
+
+        console.log('2 => ' + yourHello());
+    }
+
+    console.log('3 => ' + yourHello());
+    console.log("");
 })();
--- Chap01_JavaScript/src/A03_TemplateLiteral.js
+++ Chap01_JavaScript/src/A03_TemplateLiteral.js
@@ -1,9 +1,10 @@
 
 // ES6
-(function(){
+(function () {
 
     let name = 'NolBu';
     console.log('My Name is ' + name);
+    console.log(`My Name is ${name}`);
 
 
     let obj = {
@@ -14,14 +15,27 @@
             tel: '010-1234-5678',
             add: 'Seoul'
         },
-        toString: function(){
+        toString: function () {
             console.log(this.name + '님의 나이는 ' + this.age + '세 입니다');
 
             // ES6 - tempate 문자열
-            
+            console.log(`${this.name}님의 나이는 ${this.age}세 입니다`);
+
+            console.log(`${this.name}님의 나이는 ${this.age}세이고
+    ${this.name}님은 ${this.info.add}에 거주합니다.`);
         }
     }
-    
+
     obj.toString();
 
+    const dom = `
+        <div>
+            <h1>Hello World</h1>
+
+            <div>
+                This is Content.
+            </div>
+        </div>
+    `;
+    console.log(dom);
 })();
