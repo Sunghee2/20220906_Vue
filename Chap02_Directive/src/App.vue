2ee9a3cb6d9a8d21c46a02aaee2d87c3fbce0c47
SHINSUNGJIN <pxp13716@gmail.com>
2022-09-27 10:05:21 +0900

    .

---
+++ Chap02_Directive/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="m-3">
+    <h1>Chap02 Directive</h1>
+  
+    <div>
+      Vue 2.X에서는 template 아래에 단일 요소만 지원한다.<br>
+      Vue 3.X에서는 다중 요소 지원
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+
+</style>
