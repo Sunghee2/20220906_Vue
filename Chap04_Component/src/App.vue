c220e5065f5aad2d69b79770b7a62dca45562833
SHINSUNGJIN <pxp13716@gmail.com>
2022-09-28 16:49:23 +0900

    .

---
+++ Chap04_Component/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="m-3">
+    <h1>Chap04 Component</h1>
+
+    <A02DynamicComp></A02DynamicComp><br>
+    <A01DynamicComp></A01DynamicComp>
+  </div>
+</template>
+
+<script>
+import A01DynamicComp from './components/A01DynamicComp.vue';
+import A02DynamicComp from './components/A02DynamicComp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    A01DynamicComp, A02DynamicComp, 
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Chap04_Component/src/components/A01DynamicComp.vue
@@ -0,0 +1,34 @@
+<template>
+  <h3>A01 Dynamic Component</h3>
+
+  <button   @click="changeCurrent('A01BannerHome')">HOME</button>
+  <button   @click="changeCurrent('A01BannerAbout')">ABOUT</button>
+  <button   @click="changeCurrent('A01BannerNews')">PRODUCT</button>
+
+  <component :is="current"></component>
+</template>
+
+<script>
+import A01BannerHome from './childComps/A01BannerHome.vue'
+import A01BannerAbout from './childComps/A01BannerAbout.vue'
+import A01BannerNews from './childComps/A01BannerNews.vue'
+
+export default {
+  components: {A01BannerHome, A01BannerAbout, A01BannerNews},
+  data() {
+    return {
+      current: 'A01BannerHome'
+    }
+  },
+  methods: {
+    changeCurrent(name) {
+      this.current = name;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .banner { width: 700px; padding: 10px; border: 2px solid #CCC; border-radius: 5px; background-color: white; font-size: smaller; }
+  h5 { font-size: 150%; color: gray; }  
+</style>
