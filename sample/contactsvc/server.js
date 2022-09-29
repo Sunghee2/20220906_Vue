3ce549eb817fef3faf2dccb5cdbc63843ac52c1c
SHINSUNGJIN <pxp13716@gmail.com>
2022-09-29 12:01:00 +0900

    .

--- sample/contactsvc/server.js
+++ sample/contactsvc/server.js
@@ -1,19 +1,19 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var cors = require('cors');
+var bodyParser = require("body-parser");
+var cors = require("cors");
 //var morgan = require('morgan');
 //var path = require('path');
 //var fs = require('fs');
 //var rfs = require('rotating-file-stream');
 
 app.use(cors());
-app.enable("jsonp callback");   //jsonp 지원
+app.enable("jsonp callback"); //jsonp 지원
 app.use(function (req, res, next) {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    next()
+  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+  res.header("Expires", "-1");
+  res.header("Pragma", "no-cache");
+  next();
 });
 //-- 로깅
 /**
@@ -26,22 +26,24 @@ var accessLogStream = rfs('access.log', {
 app.use(morgan('combined', {stream: accessLogStream}))
 */
 
-app.set('port', (process.env.PORT || 8080));
+app.set("port", process.env.PORT || 8000);
 
-app.use(express.static('public'));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-const requestIp = require('request-ip');
-app.use(requestIp.mw())
+const requestIp = require("request-ip");
+app.use(requestIp.mw());
 
-var server = app.listen(app.get('port'), function() {
-    console.log("연락처 서비스가 " + app.get('port') + "번 포트에서 시작되었습니다!");
+var server = app.listen(app.get("port"), function () {
+  console.log("연락처 서비스가 " + app.get("port") + "번 포트에서 시작되었습니다!");
 });
 
-var router = require('./routes')(app);
+var router = require("./routes")(app);
